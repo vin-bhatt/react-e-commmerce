132e84759b35b708cea73456e5492f9a6d5105f6
vin-bhatt <vinayak.bhatt@quovantis.com>
2021-08-05 17:39:32 +0530

    loader functionality patched

--- src/views/Home.js
+++ src/views/Home.js
@@ -8,7 +8,7 @@ import Card from "@material-ui/core/Card";
 import { Button, Typography, Grid } from "@material-ui/core";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import ClipLoader from "react-spinners/ClimbingBoxLoader";
+import ClipLoader from "react-spinners/ClipLoader";
 import { css } from "@emotion/core";
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
@@ -60,16 +60,16 @@ const override = css`
 `;
 
 const Home = () => {
+  const productsSelector = useSelector((state) => state.products.products);
+  const loadingSelector = useSelector((state) => state.products.loading);
   const [products, setProducts] = useState([]);
   const [fields, setFields] = useState([]);
   const [searchText, setSearchText] = useState("");
   const [loading, setLoading] = useState(false);
-  const color = useState("#3f51b5");
+  let [color, setColor] = useState("#3f51b5");
   const classes = useStyles();
   const dispatch = useDispatch();
-  const productsSelector = useSelector((state) => state.products.products);
-  const loadingSelector = useSelector((state) => state.products.loading);
-
+  
   //gets the corresponding fields 
   useEffect(() => {
     setFields(getFields("products"));
@@ -118,60 +118,62 @@ const Home = () => {
   const handleChange = (value) => {
     setSearchText(value);
   };
-  return (
-    <>
-      <Grid item xs={12}>
-        <SearchInput onChange={handleChange} />
-      </Grid>
-      <br />
-      {filteredData?.map((product) => {
-        if (product)
-          return (
-            <Grid key={product.id} item xs={4}>
-              <Card className={classes.root}>
-                <CardContent>
-                  <Typography
-                    className={classes.title}
-                    color="textPrimary"
-                    gutterBottom
-                  >
-                    {product["title"]}
-                  </Typography>
-                  {fields?.map((field) => {
-                    if(field.field === "image"){
-                      return (
-                        <div className={classes.productField}>
-                          <img className={classes.image} alt={product['title']} src={product[field.field]} />
-                        </div>
-                      )
-                    }
-                    if (field.field !== "title")
-                      return (
-                        <div className={classes.productField}>
-                          <p className={classes.fieldName}>
-                            <strong>{field.label}:</strong>
-                          </p>
-                          <p className={classes.fieldValue, field.field==='description'?classes.description:null}>
-                            {product[field.field]}{field.field==='price' && ` $`}
-                          </p>
-                        </div>
-                      );
-                    else return <></>;
-                  })}
-                </CardContent>
-                <CardActions className={classes.addToCart}>
-                  <Button size="small" onClick={() => handleAddToCart(product)}>
-                    <strong>Add to Cart</strong>
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          );
-        else return <></>;
-      })}
-      <ClipLoader color={color} loading={loading} css={override} size={30} />
-    </>
-  );
+  if(!loading){
+    return (
+      <>
+        <Grid item xs={12}>
+          <SearchInput onChange={handleChange} />
+        </Grid>
+        <br />
+        {filteredData?.map((product) => {
+          if (product)
+            return (
+              <Grid key={product.id} item xs={4}>
+                <Card className={classes.root}>
+                  <CardContent>
+                    <Typography
+                      className={classes.title}
+                      color="textPrimary"
+                      gutterBottom
+                    >
+                      {product["title"]}
+                    </Typography>
+                    {fields?.map((field) => {
+                      if(field.field === "image"){
+                        return (
+                          <div className={classes.productField}>
+                            <img className={classes.image} alt={product['title']} src={product[field.field]} />
+                          </div>
+                        )
+                      }
+                      if (field.field !== "title")
+                        return (
+                          <div className={classes.productField}>
+                            <p className={classes.fieldName}>
+                              <strong>{field.label}:</strong>
+                            </p>
+                            <p className={classes.fieldValue, field.field==='description'?classes.description:null}>
+                              {product[field.field]}{field.field==='price' && ` $`}
+                            </p>
+                          </div>
+                        );
+                      else return <></>;
+                    })}
+                  </CardContent>
+                  <CardActions className={classes.addToCart}>
+                    <Button size="small" onClick={() => handleAddToCart(product)}>
+                      <strong>Add to Cart</strong>
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            );
+          else return <></>;
+        })}
+      </>
+    )
+  }
+  else return  <ClipLoader color={color} loading={true} css={override} size={30} />
 };
 
 export default Home;
